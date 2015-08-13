c065b804fa1fc2483c39d44234155935d3d068e3
Tim Crawford <crawfxrd@gmail.com>
2015-08-13 13:37:02 -0400

    Unset draw flag after display update

--- src/main.c
+++ src/main.c
@@ -70,6 +70,7 @@ int main(int argc, char *argv[])
         if (chip.Draw)
         {
             UpdateDisplay(chip.Display);
+            chip.Draw = false;
         }
 
         UpdateTimers(&chip);
